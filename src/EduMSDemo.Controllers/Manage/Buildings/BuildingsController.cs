366cbae4b1955009165ef2412be5b8cf990f3013
bs135 <thanhhai135@gmail.com>
2016-04-25 16:54:01 +0700

    add Building Controller

---
+++ src/EduMSDemo.Controllers/Manage/Buildings/BuildingsController.cs
@@ -0,0 +1,69 @@
+﻿using EduMSDemo.Components.Mvc;
+using EduMSDemo.Objects;
+using EduMSDemo.Services;
+using EduMSDemo.Validators;
+using System;
+using System.Web.Mvc;
+
+namespace EduMSDemo.Controllers.Manage
+{
+    [Area("Manage")]
+    public class BuildingsController : ValidatedController<IBuildingValidator, IBuildingService>
+    {
+        public BuildingsController(IBuildingValidator validator, IBuildingService service)
+            : base(validator, service)
+        {
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View(Service.GetViews());
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Exclude = "Id")] BuildingView model)
+        {
+            if (!Validator.CanCreate(model))
+                return View(model);
+
+            Service.Create(model);
+
+            return RedirectIfAuthorized("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Int32 id)
+        {
+            BuildingView view = Service.Get<BuildingView>(id);
+            return NotEmptyView(view);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BuildingView model)
+        {
+            if (!Validator.CanEdit(model))
+                return View(model);
+
+            Service.Edit(model);
+
+            return RedirectIfAuthorized("Index");
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult Delete(Int32 id)
+        {
+            Service.Delete(id);
+
+            return RedirectIfAuthorized("Index");
+        }
+    }
+}
---
+++ src/EduMSDemo.Controllers/Manage/ClassRooms/ClassRoomsController.cs
@@ -0,0 +1,82 @@
+﻿using EduMSDemo.Components.Mvc;
+using EduMSDemo.Objects;
+using EduMSDemo.Services;
+using EduMSDemo.Validators;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace EduMSDemo.Controllers.Manage
+{
+    [Area("Manage")]
+    public class ClassRoomsController : ValidatedController<IClassRoomValidator, IClassRoomService>
+    {
+        public ClassRoomsController(IClassRoomValidator validator, IClassRoomService service)
+            : base(validator, service)
+        {
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View(Service.GetViews());
+
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            ViewBag.BuildingId = new SelectList(Service.GetBuildingViews(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Exclude = "Id")] ClassRoomView model)
+        {
+            if (!Validator.CanCreate(model))
+            {
+                ViewBag.BuildingId = new SelectList(Service.GetBuildingViews(), "Id", "Name", model.BuildingId);
+                return View(model);
+            }
+
+            Service.Create(model);
+
+            return RedirectIfAuthorized("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Int32 id)
+        {
+            ClassRoomView view = Service.Get<ClassRoomView>(id);
+            ViewBag.BuildingId = new SelectList(Service.GetBuildingViews(), "Id", "Name", view.BuildingId);
+
+            return NotEmptyView(view);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClassRoomView model)
+        {
+            if (!Validator.CanEdit(model))
+            {
+                ViewBag.BuildingId = new SelectList(Service.GetBuildingViews(), "Id", "Name", model.BuildingId);
+                return View(model);
+            }
+
+            Service.Edit(model);
+
+            return RedirectIfAuthorized("Index");
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult Delete(Int32 id)
+        {
+            Service.Delete(id);
+
+            return RedirectIfAuthorized("Index");
+        }
+    }
+}
