dafdd8c9809757ad337f7e68e4b7f3790562435c
bs135 <thanhhai135@gmail.com>
2016-04-25 16:21:44 +0700

    update Dependency Injection

--- src/EduMSDemo.Web/App_Start/DependencyInjection/MainContainer.cs
+++ src/EduMSDemo.Web/App_Start/DependencyInjection/MainContainer.cs
@@ -52,6 +52,46 @@ namespace EduMSDemo.Web.DependencyInjection
             Register<IProductGroupService, ProductGroupService>();
             #endregion
 
+            #region Buildings service
+            Register<IBuildingService, BuildingService>();
+            Register<IClassRoomService, ClassRoomService>();
+            #endregion
+
+            #region Curriculums service
+            Register<ICurriculumDetailService, CurriculumDetailService>();
+            Register<ICurriculumService, CurriculumService>();
+            Register<ICurriculumTypeService, CurriculumTypeService>();
+            #endregion
+
+            #region Scores service
+            Register<IBonusScoreService, BonusScoreService>();
+            Register<IScoreRecordDetailService, ScoreRecordDetailService>();
+            Register<IScoreRecordService, ScoreRecordService>();
+            #endregion
+
+            #region Students service
+            Register<ICourseService, CourseService>();
+            Register<IStudentClassService, StudentClassService>();
+            Register<IStudentService, StudentService>();
+            #endregion
+
+            #region Studies service
+            Register<ISemesterService, SemesterService>();
+            Register<ISubjectClassService, SubjectClassService>();
+            Register<ISubjectClassTeacherService, SubjectClassTeacherService>();
+            #endregion
+
+            #region Subjects service
+            Register<IPreSubjectService, PreSubjectService>();
+            Register<ISubjectService, SubjectService>();
+            #endregion
+
+            #region Teachers service
+            Register<IDepartmentService, DepartmentService>();
+            Register<IFacultyService, FacultyService>();
+            Register<IFacultyManageBoardService, FacultyManageBoardService>();
+            Register<IStaffService, StaffService>();
+            #endregion
 
             // Validator
 
@@ -66,6 +106,46 @@ namespace EduMSDemo.Web.DependencyInjection
             Register<IProductGroupValidator, ProductGroupValidator>();
             #endregion
 
+            #region Buildings validator
+            Register<IBuildingValidator, BuildingValidator>();
+            Register<IClassRoomValidator, ClassRoomValidator>();
+            #endregion
+
+            #region Curriculums validator
+            Register<ICurriculumValidator, CurriculumValidator>();
+            Register<ICurriculumDetailValidator, CurriculumDetailValidator>();
+            Register<ICurriculumTypeValidator, CurriculumTypeValidator>();
+            #endregion
+
+            #region Scores validator
+            Register<IBonusScoreValidator, BonusScoreValidator>();
+            Register<IScoreRecordDetailValidator, ScoreRecordDetailValidator>();
+            Register<IScoreRecordValidator, ScoreRecordValidator>();
+            #endregion
+
+            #region Students validator
+            Register<ICourseValidator, CourseValidator>();
+            Register<IStudentClassValidator, StudentClassValidator>();
+            Register<IStudentValidator, StudentValidator>();
+            #endregion
+
+            #region Studies validator
+            Register<ISemesterValidator, SemesterValidator>();
+            Register<ISubjectClassValidator, SubjectClassValidator>();
+            Register<ISubjectClassTeacherValidator, SubjectClassTeacherValidator>();
+            #endregion
+
+            #region Subjects validator
+            Register<IPreSubjectValidator, PreSubjectValidator>();
+            Register<ISubjectValidator, SubjectValidator>();
+            #endregion
+
+            #region Teachers validator
+            Register<IDepartmentValidator, DepartmentValidator>();
+            Register<IFacultyValidator, FacultyValidator>();
+            Register<IFacultyManageBoardValidator, FacultyManageBoardValidator>();
+            Register<IStaffValidator, StaffValidator>();
+            #endregion
         }
     }
 }
