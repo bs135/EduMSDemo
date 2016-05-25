0f479e034c37640221db0582ce02971341de4dfb
bs135 <thanhhai135@gmail.com>
2016-05-25 11:37:37 +0700

    update data

--- src/EduMSDemo.Data/Migrations/201605050208577_Initial.cs
+++ src/EduMSDemo.Data/Migrations/201605050208577_Initial.cs
@@ -3,16 +3,14 @@ namespace EduMSDemo.Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class add_class_name : DbMigration
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.SubjectClass", "Name", c => c.String(nullable: false, maxLength: 128));
         }
         
         public override void Down()
         {
-            DropColumn("dbo.SubjectClass", "Name");
         }
     }
 }
